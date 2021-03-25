980da1645063e4b648da91e787fd15bf7fc76f23
yaswanth-pula <yaswanth.pula@gmail.com>
2021-03-25 21:19:01 +0530

    Completed Finding Unique IP Assignment

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -30,6 +30,52 @@ public class LogAnalyzer
              System.out.println(le);
          }
      }
-     
-     
+
+     public int countUniqueIPs(){
+         ArrayList<String> uniqueIpList = new ArrayList<>();
+         for(LogEntry logEntry : records){
+             String logIp = logEntry.getIpAddress();
+             if(! uniqueIpList.contains(logIp))
+                    uniqueIpList.add(logIp);
+         }
+         return uniqueIpList.size();
+     }
+
+     public void printAllHigherThanNum(int minStatusCode){
+            for(LogEntry logEntry : records){
+                int currentLogStatusCode = logEntry.getStatusCode();
+                if(currentLogStatusCode > minStatusCode)
+                    System.out.println(logEntry);
+            }
+     }
+
+     public ArrayList<String> uniqueIPVisitsOnDay(String someDay){
+         ArrayList<String> uniqueIpList = new ArrayList<>();
+
+         for(LogEntry logEntry : records){
+             String logIp = logEntry.getIpAddress();
+             String logDay = logEntry.getAccessTime().toString();
+             if( (! uniqueIpList.contains(logIp) ) && logDay.contains(someDay) )
+                 uniqueIpList.add(logIp);
+         }
+         return uniqueIpList;
+     }
+
+     public int countUniqueIPsInRange(int minStatusCode, int maxStatusCode){
+         ArrayList<String> uniqueIpList = new ArrayList<>();
+         for(LogEntry logEntry : records){
+             String logIp = logEntry.getIpAddress();
+             int currentLogStatusCode = logEntry.getStatusCode();
+             boolean isInRange = currentLogStatusCode >= minStatusCode
+                                && currentLogStatusCode <= maxStatusCode;
+
+             if(! uniqueIpList.contains(logIp) && isInRange) {
+                    uniqueIpList.add(logIp);
+             }
+         }
+         return uniqueIpList.size();
+     }
+
+
+
 }
--- Tester.java
+++ Tester.java
@@ -16,15 +16,44 @@ public class Tester
         LogEntry le2 = new LogEntry("1.2.100.4", new Date(), "example request 2", 300, 400);
         System.out.println(le2);
     }
-    
+
     public void testLogAnalyzer() {
         LogAnalyzer logAnalyzer = new LogAnalyzer();
         logAnalyzer.readFile("src/LogFiles/short-test_log");
         logAnalyzer.printAll();
     }
+    public void testUniqueIP(){
+        LogAnalyzer logAnalyzer = new LogAnalyzer();
+        logAnalyzer.readFile("src/LogFiles/short-test_log");
+        System.out.println(logAnalyzer.countUniqueIPs());
+    }
+    public void testHigherThanNum(){
+        LogAnalyzer logAnalyzer = new LogAnalyzer();
+        logAnalyzer.readFile("src/LogFiles/short-test_log");
+        logAnalyzer.printAllHigherThanNum(200);
+    }
+    public void testUniqueIPOnDay(){
+        LogAnalyzer logAnalyzer = new LogAnalyzer();
+        logAnalyzer.readFile("src/LogFiles/weblog-short_log");
+        ArrayList<String> uniqueIPVisitList = logAnalyzer.uniqueIPVisitsOnDay("Sep 30");
+        for(String ip : uniqueIPVisitList){
+            System.out.println(ip);
+        }
+
+    }
+    public void testUniqueCountRange(){
+        LogAnalyzer logAnalyzer = new LogAnalyzer();
+        logAnalyzer.readFile("src/LogFiles/short-test_log");
+        System.out.println(logAnalyzer.countUniqueIPsInRange(200,299));
+        System.out.println(logAnalyzer.countUniqueIPsInRange(300,399));
+    }
 
     public static void main(String[] args) {
         Tester tester = new Tester();
-        tester.testLogAnalyzer();
+//        tester.testLogAnalyzer();
+//        tester.testUniqueIP();
+//        tester.testHigherThanNum();
+//        tester.testUniqueIPOnDay();
+//        tester.testUniqueCountRange();
     }
 }
