997cce65f77de5d3b6e5aaa08e2f351a5e3b7887
yaswanth-pula <yaswanth.pula@gmail.com>
2021-03-31 10:51:39 +0530

    Added error stream code for failed ping

--- Assignment3.java
+++ Assignment3.java
@@ -4,60 +4,72 @@ import java.util.*;
 /**
  * Write a java function that will ping any host ( given as input )
  * and computes the median of the time taken to ping.
- *
+ * <p>
  * Use the system ping utility, do not use any deprecated methods.
- *
- *
- * */
+ */
 
 class PingMedian {
 
-    public void printPingMedian(String host, int pingCount) throws IOException{
-        if(host == null) return;
+    public void printPingMedian(String host, int pingCount) {
+        if (host == null) return;
 
-        double[] pingTimings = getPingTime(host,pingCount);
-        Arrays.sort(pingTimings);
-
-        if(pingCount % 2 != 0) {
-            System.out.println(pingTimings[(pingCount / 2)]);
+        double[] pingTimings = new double[pingCount];
+        try {
+            pingTimings = getPingTime(host, pingCount);
+        } catch (IOException exception) {
+            System.out.println("0.0");
         }
-        else {
-            double mid1 = pingTimings[((pingCount - 1 )/ 2)];
-            double mid2 = pingTimings[((pingCount + 1 )/ 2)];
-            System.out.println((mid1 + mid2 )/ 2.0);
+
+        Arrays.sort(pingTimings);
+        System.out.print("Ping Median for " + host + " is : ");
+
+        if (pingCount % 2 != 0) {
+            System.out.println(pingTimings[(pingCount / 2)]);
+        } else {
+            double mid1 = pingTimings[((pingCount - 1) / 2)];
+            double mid2 = pingTimings[((pingCount + 1) / 2)];
+            System.out.println((mid1 + mid2) / 2.0);
         }
     }
 
-    private double[] getPingTime(String host,int pingCount) throws IOException {
+    private double[] getPingTime(String host, int pingCount) throws IOException {
         double[] pingTimings = new double[pingCount];
-        String[] commandList = {"ping",host,"-c",String.valueOf(pingCount)};
+        String[] commandList = {"ping", host, "-c", String.valueOf(pingCount)};
         ProcessBuilder processBuilder = new ProcessBuilder(commandList);
         Process process = processBuilder.start();
 
         ArrayList<String> pingResult = new ArrayList<>();
-        Scanner scanner = new Scanner(process.getInputStream());
+        Scanner inputScanner = new Scanner(process.getInputStream());
+        Scanner outputScanner = new Scanner(process.getErrorStream());
 
-        while(scanner.hasNextLine()){
-            String resultLine  = scanner.nextLine();
+        while (outputScanner.hasNextLine())
+            System.out.println(outputScanner.nextLine());
+
+        while (inputScanner.hasNextLine()) {
+            String resultLine = inputScanner.nextLine();
             int timeStringIndex = resultLine.indexOf("time=");
-            if(timeStringIndex != -1) {
-                pingResult.add(resultLine.substring(timeStringIndex+5));
+            if (timeStringIndex != -1) {
+                pingResult.add(resultLine.substring(timeStringIndex + 5));
             }
         }
 
-        for(int index = 0 ;index < pingResult.size();index++){
+        for (int index = 0; index < pingResult.size(); index++) {
             String currentTimeString = pingResult.get(index);
             double currentPingValue = Double.parseDouble(currentTimeString.split(" ")[0]);
 
             pingTimings[index] = currentPingValue;
         }
+
         return pingTimings;
     }
 
 }
+
 public class Assignment3 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         PingMedian median = new PingMedian();
-        median.printPingMedian("8.8.8.8",5);
+        median.printPingMedian("not a host", 3);
+        median.printPingMedian("8.8.8.8", 5);
+        median.printPingMedian("www.youtube.com", 8);
     }
 }
